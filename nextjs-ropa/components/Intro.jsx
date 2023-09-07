8b18485f2b56569fc60dcbe03887cc608b33bb4f
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-09-06 22:14:44 -0300

    intro de pagina

--- nextjs-ropa/components/Intro.jsx
+++ nextjs-ropa/components/Intro.jsx
@@ -2,13 +2,25 @@ import "../app/css/productos.css"
 import "../app/css/slider.css"
 import "../app/css/style.css"
 
-export default function Intro() {
+const Intro = () => {
   return (
-    <section class="w-50 mx-auto text-center pt-4" id="intro">
-    <h1 class="p-3 fs-1 border-top border-3 ">Bienvenido a todos nuestros productos
-      <br /><span class="text-primary serLibre"> ERES LIBRE</span>
-    </h1>
-    <p class="p-3 fs-4"><span class="text-primary">VERANO 2023 </span> Pequeña introducción a nuestro catalogo</p>
-  </section>
-  )
+    <section className="container text-center py-4" id="intro">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1 className="display-4 pb-3">
+            Bienvenido a todos nuestros productos
+            <br />
+            <span className="text-primary">ERES LIBRE</span>
+          </h1>
+          <p className="lead">
+            <span className="text-success">VERANO 2023</span> Pequeña introducción a nuestro catálogo
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
+
+export default Intro;
+
+

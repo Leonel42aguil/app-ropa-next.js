834ecb3d44b912b192f154f8fddaaaba9d1a1ce0
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-09-06 23:04:21 -0300

    Seccion Productos de Ropa

--- nextjs-ropa/components/Products.jsx
+++ nextjs-ropa/components/Products.jsx
@@ -1,36 +1,38 @@
 
 export default function Products() {
   return (
-<section className="ofert container">
-        <div className="ofert-1">
-            <div className="ofert-img">
-                {/* <img src="images/1.png" alt=""/> */}
-            </div>
-            <div class="ofert-text">
-                <h3>Producto 1</h3>
-                <a href="#" class="btn-2">Producto 1</a>
-            </div>
+    <section class="ofert"> 
+    <div class="ofert-1"> 
+        <div class="ofert-img"> 
+            {/* <img src="images/f1.png" alt=""/> */}
         </div>
+        <div class="ofert-text">
+            <h3>Producto 1</h3>
+            <a href="#" class="btn-2">Producto 1</a>
+        </div>
+    </div>
 
-        <div className="ofert-1">
-            <div className="ofert-img">
-                {/* <img src="images/2.png" alt=""/> */}
-            </div>
-            <div class="ofert-text">
-                <h3>Producto 1</h3>
-                <a href="#" class="btn-2">Producto 2</a>
-            </div>
+    <div class="ofert-1"> 
+        <div class="ofert-img"> 
+            {/* <img src="images/f2.png" alt=""/> */}
         </div>
+        <div class="ofert-text">
+            <h3>Producto 1</h3>
+            <a href="#" class="btn-2">Producto 2</a>
+        </div>
+    </div>
 
-        <div className="ofert-1">
-            <div className="ofert-img">
-                {/* <img src="images/3.png" alt=""/> */}
-            </div>
-            <div class="ofert-text">
-                <h3>Producto 1</h3>
-                <a href="#" class="btn-2">Producto 3</a>
-            </div>
+    <div class="ofert-1"> 
+        <div class="ofert-img"> 
+            {/* <img src="images/f3.png" alt=""/> */}
         </div>
-    </section>
+        <div class="ofert-text">
+            <h3>Producto 1</h3>
+            <a href="#" class="btn-2">Producto 3</a>
+        </div>
+    </div>
+</section>
+
   )
 }
+

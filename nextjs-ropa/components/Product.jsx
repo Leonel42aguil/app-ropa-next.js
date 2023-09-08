ee48f544702086513d87926d9345375d3c87e208
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-09-08 18:18:19 -0300

    Funcion-Product

--- nextjs-ropa/components/Product.jsx
+++ nextjs-ropa/components/Product.jsx
@@ -5,7 +5,6 @@ import "../app/css/slider.css";
 import "../app/css/style.css";
 import "./Products.css";
 import React, { useState } from "react";
-import { Carousel } from "react-bootstrap";
 
 export default function Product() {
   const [index, setIndex] = useState(0);
@@ -13,91 +12,169 @@ export default function Product() {
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
+  
+  const [showCards, setShowCards] = useState({
+    cintos: false,
+    calzados: false,
+    carteras: false,
+    camisas: false,
+    camperas: false,
+    buzos: false,
+  });
+
+  const toggleCards = (product) => {
+    // Oculta todas las tarjetas
+    setShowCards({
+      cintos: false,
+      calzados: false,
+      carteras: false,
+      camisas: false,
+      camperas: false,
+      buzos: false,
+      [product]: true, // Muestra la tarjeta del producto clicado
+    });
+  };
+
+  const [activeCard, setActiveCard] = useState(null);
+
+  const toggleCard = (product) => {
+    setActiveCard(product === activeCard ? null : product);
+  };
 
   return (
-    <section class="ofert">
-      <div class="ofert-1">
-        <div class="ofert-product">
-          <div class="ofert-img">
+    <section className="ofert">
+      <div className={`ofert-1 ${activeCard === 'cintos' ? 'active' : ''}`}>
+        <div className="ofert-product">
+          <div className="ofert-img">
             <img src="/image/f1.png" alt="product1" />
           </div>
-          <div class="ofert-text">
+          <div className="ofert-text">
             <h3>Cintos</h3>
-            <a href="#" class="btn-2">
-              Comprar Producto 1
-            </a>
+            <button className="btn-2" onClick={() => toggleCard('cintos')}>
+              Comprar
+            </button>
           </div>
         </div>
       </div>
 
-      <div class="ofert-1">
-        <div class="ofert-product">
-          <div class="ofert-img">
+      <div className={`ofert-1 ${activeCard === 'calzados' ? 'active' : ''}`}>
+        <div className="ofert-product">
+          <div className="ofert-img">
             <img src="/image/f2.png" alt="product2" />
           </div>
-          <div class="ofert-text">
+          <div className="ofert-text">
             <h3>Calzados</h3>
-            <a href="#" class="btn-2">
-              Comprar Producto 2
-            </a>
+            <button className="btn-2" onClick={() => toggleCard('calzados')}>
+              Comprar
+            </button>
           </div>
         </div>
       </div>
 
-      <div class="ofert-1">
-        <div class="ofert-product">
-          <div class="ofert-img">
+      <div className={`ofert-1 ${activeCard === 'carteras' ? 'active' : ''}`}>
+        <div className="ofert-product">
+          <div className="ofert-img">
             <img src="/image/f3.png" alt="product3" />
           </div>
-          <div class="ofert-text">
+          <div className="ofert-text">
             <h3>Carteras</h3>
-            <a href="#" class="btn-2">
-              Comprar Producto 3
-            </a>
+            <button className="btn-2" onClick={() => toggleCard('carteras')}>
+              Comprar
+            </button>
           </div>
         </div>
       </div>
 
-      <div class="ofert-1">
-        <div class="ofert-product">
-          <div class="ofert-img">
+      <div className={`ofert-1 ${activeCard === 'camisas' ? 'active' : ''}`}>
+        <div className="ofert-product">
+          <div className="ofert-img">
             <img src="/image/4.png" alt="product4" />
           </div>
-          <div class="ofert-text">
+          <div className="ofert-text">
             <h3>Camisas</h3>
-            <a href="#" class="btn-2">
-              Comprar Producto 4
-            </a>
+            <button className="btn-2" onClick={() => toggleCard('camisas')}>
+              Comprar
+            </button>
           </div>
         </div>
       </div>
 
-      <div class="ofert-1">
-        <div class="ofert-product">
-          <div class="ofert-img">
-            <img src="/image/4.png" alt="product4" />
+      <div className={`ofert-1 ${activeCard === 'camperas' ? 'active' : ''}`}>
+        <div className="ofert-product">
+          <div className="ofert-img">
+            <img src="/image/5.png" alt="product5" />
           </div>
-          <div class="ofert-text">
+          <div className="ofert-text">
             <h3>Camperas</h3>
-            <a href="#" class="btn-2">
-              Comprar Producto 4
-            </a>
+            <button className="btn-2" onClick={() => toggleCard('camperas')}>
+              Comprar
+            </button>
           </div>
         </div>
       </div>
-      <div class="ofert-1">
-        <div class="ofert-product">
-          <div class="ofert-img">
-            <img src="/image/4.png" alt="product4" />
+
+      <div className={`ofert-1 ${activeCard === 'buzos' ? 'active' : ''}`}>
+        <div className="ofert-product">
+          <div className="ofert-img">
+            <img src="/image/6.png" alt="product6" />
           </div>
-          <div class="ofert-text">
+          <div className="ofert-text">
             <h3>Buzos</h3>
-            <a href="#" class="btn-2">
-              Comprar Producto 4
-            </a>
+            <button className="btn-2" onClick={() => toggleCard('buzos')}>
+              Comprar
+            </button>
           </div>
         </div>
       </div>
+      
+      {/* Tarjetas de detalles */}
+      {activeCard === 'cintos' && (
+        <div className="ofert-1">
+          <div className="ofert-card">
+            <p>Detalles de Cintos</p>
+          </div>
+        </div>
+      )}
+
+      {activeCard === 'calzados' && (
+        <div className="ofert-1">
+          <div className="ofert-card">
+            <p>Detalles de Calzados</p>
+          </div>
+        </div>
+      )}
+
+      {activeCard === 'carteras' && (
+        <div className="ofert-1">
+          <div className="ofert-card">
+            <p>Detalles de Carteras</p>
+          </div>
+        </div>
+      )}
+
+      {activeCard === 'camisas' && (
+        <div className="ofert-1">
+          <div className="ofert-card">
+            <p>Detalles de Camisas</p>
+          </div>
+        </div>
+      )}
+
+      {activeCard === 'camperas' && (
+        <div className="ofert-1">
+          <div className="ofert-card">
+            <p>Detalles de Camperas</p>
+          </div>
+        </div>
+      )}
+
+      {activeCard === 'buzos' && (
+        <div className="ofert-1">
+          <div className="ofert-card">
+            <p>Detalles de Buzos</p>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
